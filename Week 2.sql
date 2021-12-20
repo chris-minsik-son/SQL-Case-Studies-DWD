74dc3569524a9297db45c9799cb294f5e4617311
Min-Sik Son <chris.minsik.son@gmail.com>
2021-12-20 17:04:04 +1100

    Week 2 ...

---
+++ Week 2.sql
@@ -0,0 +1,75 @@
+-- WEEK 2: DATA EXPLORATION EXERCISES
+
+-- RECORD COUNTS & DISTINCT VALUES
+
+-- (1) Which id value has the most number of duplicate records in the health.user_logs table?
+
+-- (2) Which log_date value had the most duplicate records after removing the max duplicate id value from question 1?
+
+-- (3) Which measure_value had the most occurences in the health.user_logs value when measure = 'weight'?
+
+-- (4) How many single duplicated rows exist when measure = 'blood_pressure' in the health.user_logs? How about the total number of duplicate records in the same table?
+
+-- (5) What percentage of records measure_value = 0 when measure = 'blood_pressure' in the health.user_logs table? How many records are there also for this same condition?
+
+-- (6) What percentage of records are duplicates in the health.user_logs table?
+
+
+
+-- Exploring the Health Analytics Data:
+SELECT * FROM health.user_logs LIMIT 10;
+
+-- How many rows in the data?
+SELECT COUNT(*) FROM health.user_logs;
+
+-- Count on the unique entries in the measures column
+SELECT
+    measure,
+    COUNT(*) AS frequency
+FROM health.user_logs
+GROUP BY measure;
+
+-- Number of unique customers
+SELECT COUNT(DISTINCT id) FROM health.user_logs;
+
+-- Find the top 10 customers by record count
+SELECT
+    id,
+    COUNT(*) AS row_count
+FROM health.user_logs
+GROUP BY id
+ORDER BY row_count DESC
+LIMIT 10;
+
+-- Inspecting data where measure_value = 0
+SELECT *
+FROM health.user_logs
+WHERE measure_value = 0;
+
+SELECT
+    measure,
+    COUNT(*) AS record_count
+FROM health.user_logs
+WHERE measure_value = 0
+GROUP BY measure;
+
+-- Checking NULL values in the systolic column
+SELECT COUNT(*)
+FROM health.user_logs
+WHERE systolic IS NULL;
+
+-- Removing duplicates and counting unique entries
+-- CTE: (preferred method over subquery)
+WITH deduped_logs AS (
+    SELECT DISTINCT *
+    FROM health.user_logs
+)
+SELECT COUNT(*)
+FROM deduped_logs;
+
+-- Subquery:
+SELECT COUNT(*)
+FROM (
+    SELECT DISTINCT *
+    FROM health.user_logs
+) AS subquery;
